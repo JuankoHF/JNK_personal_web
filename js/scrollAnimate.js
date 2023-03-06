23aa9a11615b838230fd369d6fa1cc45469073b3
JuankoHF <108952217+JuankoHF@users.noreply.github.com>
2023-03-06 22:11:09 +0100

    cambios para animar con scroll

---
+++ js/scrollAnimate.js
@@ -0,0 +1,33 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.name_section', {
+    duration: 2500,
+    origin: 'bottom',
+    distance: '-100px'
+});
+
+sr.reveal('.moved', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '200px'
+});
+
+sr.reveal('.more', {
+    duration: 3000,
+    rotate: {
+        x: 90,
+    }
+});
+
+sr.reveal('.tocontact', {
+    duration: 2000,
+    origin: 'right',
+    distance: '200px'
+});
+sr.reveal('.span_content', {
+    duration: 3000,
+    rotate: {
+        y: 90,
+    }
+});
+
