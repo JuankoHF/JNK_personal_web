e15205676cc3bfcb2cb6e231abac4995e2a5572c
JuankoHF <108952217+JuankoHF@users.noreply.github.com>
2023-03-07 16:53:23 +0100

    mejoras a animacion scroll

--- js/scrollAnimate.js
+++ js/scrollAnimate.js
@@ -1,31 +1,62 @@
 window.sr = ScrollReveal();
 
-sr.reveal('.name_section', {
+sr.reveal('.about', {
     duration: 2500,
     origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('.name_section', {
+    duration: 3000,
+    origin: 'bottom',
     distance: '-100px'
+});
+
+sr.reveal('.center_present', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '100px'
 });
 
 sr.reveal('.moved', {
     duration: 2000,
     origin: 'bottom',
-    distance: '200px'
+    distance: '150px'
 });
 
 sr.reveal('.more', {
-    duration: 3000,
+    duration: 2500,
     rotate: {
         x: 90,
     }
 });
 
-sr.reveal('.tocontact', {
+sr.reveal('.portafolio', {
+    duration: 2500,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('.head_contact', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '150px'
+});
+
+sr.reveal('.toMail', {
     duration: 2000,
     origin: 'right',
-    distance: '200px'
+    distance: '20px'
 });
+
+sr.reveal('.tophone', {
+    duration: 2000,
+    origin: 'right',
+    distance: '20px'
+});
+
 sr.reveal('.span_content', {
-    duration: 3000,
+    duration: 2500,
     rotate: {
         y: 90,
     }
